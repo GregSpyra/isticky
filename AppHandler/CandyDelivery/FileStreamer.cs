3f5db9b9fd2d83c10ae115157af9f4ecaf02aedb
Gregor.Spyra@googlemail.com <Gregor.Spyra@googlemail.com@0dbfe80f-a0b6-567f-1f5f-34dbf0a79237>
2015-02-08 17:38:05 +0000

    CHANGE: G$; ooxml integration

---
+++ AppHandler/CandyDelivery/FileStreamer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pep.AppHandler.CandyDelivery
+{
+	public class FileStreamer : IDisposable
+	{
+		#region Variables
+		private bool disposed = false;
+		private string sqlConnectionString;
+		private SqlConnection sqlConnection;
+		#endregion
+
+		#region Properties
+		public SqlConnection SqlConnection
+		{
+			get
+			{
+				return this.sqlConnection;
+			}
+		}
+		#endregion
+
+		#region Constructors
+		public FileStreamer(string sqlConnectionString)
+		{
+			this.sqlConnectionString = sqlConnectionString;
+			this.sqlConnection = new SqlConnection(sqlConnectionString);
+		}
+		#endregion
+
+		#region IDisposable Members
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!this.disposed)
+			{
+				if (disposing)
+				{
+					if (this.sqlConnection != null)
+						this.sqlConnection.Dispose();
+				}
+
+				this.disposed = true;
+			}
+		}
+
+		#endregion
+	}
+}
---
+++ AppHandler/CandyStore/Candy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pep.AppHandler.CandyStore
+{
+	/// <summary>
+	/// Class provides information about different documents (candies) supported
+	/// </summary>
+	public class Candy
+	{
+		#region Enums
+		/// <summary>
+		/// Enumerator for supported document types
+		/// </summary>
+		enum Type
+		{
+			OOXML = 0x01,
+			PDF = 0x02
+		};
+		#endregion
+	}
+}
